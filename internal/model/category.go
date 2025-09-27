4d020482c0fa33902f1bb930644b5ef3df0f9575
Snowykami <snowykami@outlook.com>
2025-09-27 23:05:37 +0800

    feat: 删除旧的分类模型，简化代码结构

--- internal/model/category.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gorm.io/gorm"
-
-type Category struct {
-	gorm.Model
-	Name        string `gorm:"type:text;not null"`
-	Description string `gorm:"type:text;not null"` // 分类描述
-}
