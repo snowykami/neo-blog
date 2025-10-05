5f4a53c742036d74c873e30133cea150d3253e9d
Snowykami <snowykami@outlook.com>
2025-10-06 00:41:09 +0800

    fix: 设置 Gravatar URL 函数的默认代理为 cn.gravatar.com

--- web/src/utils/common/gravatar.ts
+++ web/src/utils/common/gravatar.ts
@@ -10,7 +10,7 @@ import type { User } from '@/models/user';
 // 256	用户资料页
 // 512	最大支持尺寸（上传原图也限于此）
 
-export function getGravatarUrl({ email, size, proxy }: { email: string, size?: number, proxy?: string }): string {
+export function getGravatarUrl({ email, size, proxy="cn.gravatar.com" }: { email: string, size?: number, proxy?: string }): string {
   const hash = md5(email.trim().toLowerCase());
   return `https://${proxy ? proxy : "www.gravatar.com"}/avatar/${hash}?s=${size}&d=identicon`;
 }
