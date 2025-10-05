352b414dc030bc85fe2156aa5b241661ea98a650
Snowykami <snowykami@outlook.com>
2025-10-06 03:40:35 +0800

    Update layout-size.ts

--- web/src/utils/common/layout-size.ts
+++ web/src/utils/common/layout-size.ts
@@ -4,7 +4,7 @@ export const navStickyTop = navHeight + 4; // px, 4是间距
 export const navStickyTopPx = navStickyTop * 4; // px, 1rem = 4px
 
 export const contentAreaMaxWidthClass = "max-w-7xl"; 
-export const contentAreaPaddingClass = "md:px-8 px-6";
+export const contentAreaPaddingClass = "md:px-8 px-4";
 export const navAreaPaddingClass = "md:px-8 px-0";
 
 export const blogPostWithTransparentNavScrollMaxHeight = 200
